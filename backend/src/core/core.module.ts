ee29876b68abb7252edebf90397a727dc11ef27d
esteban <estebangagneur03@gmail.com>
2026-01-24 16:59:21 +0100

    config module

--- backend/src/core/core.module.ts
+++ backend/src/core/core.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [DatabaseModule, EventEmitterModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    EventEmitterModule.forRoot()
+  ],
 })
 export class CoreModule {}
