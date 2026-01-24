eb868ec5999b059a5b507bcab85da932a2a06dad
esteban <estebangagneur03@gmail.com>
2026-01-24 16:55:29 +0100

    log mongo

--- backend/src/core/database/database.module.ts
+++ backend/src/core/database/database.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+console.log(process.env.MONGODB_URI)
+
 @Module({
   imports: [
     MongooseModule.forRoot(
-      process.env.MONGODB_URI ||
+      process.env.MONGODB_URI ??
         'mongodb://admin:strongpassword@localhost:27017/minecraft-wiki?authSource=admin',
     ),
   ],
